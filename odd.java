5149d8e64bc25ee8fd805f571954325b59b616d5
keerthiilango <keerthii1997@gmail.com>
2017-06-08 17:56:15 +0530

    Create odd.java

--- odd.java
+++ odd.java
@@ -1,1 +1,18 @@
-AKFN
+import java.io.*;
+import java.util.*;
+public class even
+{
+ public static void main(String args[])
+{
+  Scanner s=new Scanner(System.in);
+  int n=s.nextInt();
+  if(n%2==0)
+  {
+   System.out.println("even number");
+  }
+  else 
+  {
+   System.out.println("odd number");
+  
+  }
+ } 
