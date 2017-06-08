844282fe58e63b5286add6886b062cdce3edebc6
keerthiilango <keerthii1997@gmail.com>
2017-06-08 17:46:02 +0530

    Create even.java

--- even.java
+++ even.java
@@ -6,18 +6,13 @@ public class even
 {
   Scanner s=new Scanner(System.in);
   int n=s.nextInt();
-  if((n%2==0)&&(n!=0))
+  if(n%2==0)
   {
    System.out.println("even number");
   }
-  else if(n!=0)
+  else 
   {
    System.out.println("odd number");
-  }
-  else
-  {
-   System.out.println("zero");
-  }
   
   }
  } 
